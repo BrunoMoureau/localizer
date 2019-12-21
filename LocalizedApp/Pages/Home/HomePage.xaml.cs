7fdef91f733ce6c486ef4bfc85992d341abf891f
Bruno Moureau <bruno.moureau@hotmail.com>
2019-12-21 23:42:58 +0100

    removed debug line + updated XF to 4.4

--- LocalizedApp/Pages/Home/HomePage.xaml.cs
+++ LocalizedApp/Pages/Home/HomePage.xaml.cs
@@ -38,9 +38,6 @@ namespace LocalizedApp.Pages.Home
 
         public void OnCultureChanged(object sender, CultureInfo cultureInfo)
         {
-            // todo find why UWP isn't updating displayed texts without this trace line
-            Trace.WriteLine($"Update culture of {nameof(HomePage)}");
-
             GreetingsLabel.Text = AppResources.HomePage_GreetingsText;
             CurrentCultureLabel.Text = AppResources.HomePage_CurrentCultureText;
             SelectCultureButton.Text = AppResources.HomePage_SelectCultureText;
